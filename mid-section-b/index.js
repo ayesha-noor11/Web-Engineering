fa316837482de6f56cd8ae29ccf19ed0efcf2c79
ayesha-noor11 <157089343+ayesha-noor11@users.noreply.github.com>
2024-04-02 15:01:15 +0500

    Add files via upload

---
+++ mid-section-b/index.js
@@ -0,0 +1,16 @@
+for(i=0; i<4; i++){const image = document.getElementsById("img");}
+
+const pbtn = document.getElementsByClassName("prev");
+
+pbtn.onclick=prevbutton;
+
+function prevbutton{
+    
+}
+
+const nbtn = document.getElementsByClassName("next");
+
+const div = document.createElement("div");
+div.appendChild(image[0]);
+
+
